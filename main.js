e287d844370a1ce35a600564f381376a52fdca86
eiji mishiro <eiji346g@gmail.com>
2017-11-03 16:28:29 +0900

    init seton

---
+++ main.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const url = require('url')
+import { app, BrowserWindow } from 'electron'
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 800, hight: 600});
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('close', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', function() {
+  if(mainwindow === null) {
+    createWindow();
+  }
+});
