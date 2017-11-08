4e7d75340992d0900010d3f26b528be7d84e3a10
eiji mishiro <eiji346g@gmail.com>
2017-11-08 20:42:29 +0900

    change webpack to transpile jsx and some syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 module.exports = {
-  entry: './src/electron/app.js',
+  entry: {
+    'main': './src/electron/app.js',
+    'r_main': './src/react/app.jsx',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.join(__dirname, 'public/js')
   },
   target: 'electron-main',
@@ -11,6 +14,19 @@ module.exports = {
     fs: 'empty',
     __dirname: false,
     __filename: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader', options: {
+            presets: ['react', 'stage-0']}
+          }
+        ],
+      }
+    ]
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
