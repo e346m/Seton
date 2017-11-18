ea38f4a1db9984a8020d36271c332c23be469b84
eiji mishiro <eiji346g@gmail.com>
2017-11-18 12:47:19 +0900

    get avatar and comments

--- src/react/components/Issue.jsx
+++ src/react/components/Issue.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { withStyles } from 'material-ui/styles'
+import Avatar from 'material-ui/Avatar';
+
+const styles = {
+  avatar: {
+    margin: 10,
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+};
 
 class Issue extends React.Component {
   render() {
+    const { classes } = this.props
     const { loading, errors, node } = this.props.data
+    console.log(this.props.data)
     if (loading) {
       return(<div>{console.log("loading")}</div>)
     }
     return (
       <div>
-        <span dangerouslySetInnerHTML={_html: node.bodyHTML} />
+        <Avatar alt={node.author.login} src={node.author.avatarUrl} className={classes.bigAvatar} />
+        <div>{node.title}</div>
+        <div dangerouslySetInnerHTML={ {__html: node.bodyHTML} } />
+        { node.comments.edges.map(comment =>
+          <div>
+            <Avatar alt={comment.node.author.login} src={comment.node.author.avatarUrl} className={classes.avatar} />
+            <div dangerouslySetInnerHTML={ {__html: comment.node.bodyHTML} } />
+          </div>
+        )}
       </div>
     )
   }
@@ -22,12 +43,18 @@ const IssueQuery = gql`
     node(id: $id) {
       ... on Issue {
         title
-        author
+        author {
+          login
+          avatarUrl(size: 128)
+        }
         bodyHTML
         comments(first: 10) {
           edges {
             node {
-              author
+              author {
+                login
+                avatarUrl(size: 44)
+              }
               bodyHTML
             }
           }
@@ -41,6 +68,6 @@ const IssueWithQuery = graphql(IssueQuery, {
   options: ownProps => ({
     variables: { id: ownProps.match.params.id }
   }),
-})(Issue)
+})(withStyles(styles)(Issue))
 
 export default IssueWithQuery
